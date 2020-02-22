592e00fc09b4378809833c272b717194feb6eb22
Dolphine Atieno Awuor <awuordolphine65@gmail.com>
2020-02-22 22:00:08 +0300

    add variables in js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-/*$(document).ready(function(){
-    $("#service1").click(function(){
-        $(".de").toggle();
-    })
-})*/
+let service1=("service1")
+let de = ("de")
 $(document).ready(function(){
     $("#service1").click(function(){
       $(this).hide();
