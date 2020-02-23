da01d2ac6f956bf8040c2934e6b2de0b0939192a
Dolphine Atieno Awuor <awuordolphine65@gmail.com>
2020-02-23 06:57:37 +0300

    add id in class column-md-2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
-let service1=("service1")
-let de = ("de")
+let service1=$("service1");
+let de = $("de");
+let dev = $("dev");
+let devep = $("devep");
+let ment = $("ment");
+let proment = $("proment")
+
 $(document).ready(function(){
     $(".service1").click(function(){
       $(this).hide();
       $(".de").show();
-
     });
     $(".de").click(function(){
       $(this).hide();
       $(".service1").show();
     });
   });
+
+$(document).ready(function(){
+    $(".dev").click(function(){
+      $(this).hide();
+      $(".devep").show();
+    });
+    $(".devep").click(function(){
+      $(this).hide();
+      $(".dev").show();
+    });
+  });
+
+$(document).ready(function(){
+    $(".ment").click(function(){
+      $(this).hide();
+      $(".proment").show();
+    });
+    $(".proment").click(function(){
+      $(this).hide();
+      $(".ment").show();
+    });
+  });
+
+  //hover images
+  $('.port').mouseenter(function(){
+    $('.porttext').css('background-color', "yellow");
+ }, function(){
+    $('.porttext',this).slideToggle('slow');
+ });
